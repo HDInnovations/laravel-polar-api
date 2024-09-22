6019fcd08c6d05aef9cb94958f17593cd8be7955
HDVinnie <hdinnovations@protonmail.com>
2024-09-22 17:28:40 -0400

    add: SubscriptionClient Test

---
+++ tests/Feature/SubscriptionClientTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+use HDInnovations\LaravelPolarApi\Clients\SubscriptionClient;
+use HDInnovations\LaravelPolarApi\Exceptions\PolarApiUnprocessableEntityException;
+use Illuminate\Support\Facades\Http;
+
+it('gets subscriptions successfully', function (): void {
+    Http::fake([
+        '*' => Http::response(['data' => 'subscriptions'], 200),
+    ]);
+
+    $client = new SubscriptionClient('*', 'string');
+
+    $response = $client->getSubscriptions('org-id');
+
+    expect($response)->toBe(['data' => 'subscriptions']);
+});
+
+it('throws PolarApiUnprocessableEntityException on 422 status when getting subscriptions', function (): void {
+    Http::fake([
+        '*' => Http::response([
+            'detail' => [
+                [
+                    'loc'  => ['string'],
+                    'msg'  => 'string',
+                    'type' => 'string',
+                ],
+            ],
+        ], 422),
+    ]);
+
+    $client = new SubscriptionClient('*', 'string');
+
+    $this->expectException(PolarApiUnprocessableEntityException::class);
+    $this->expectExceptionMessage('Unprocessable Entity');
+
+    $client->getSubscriptions('org-id');
+});
